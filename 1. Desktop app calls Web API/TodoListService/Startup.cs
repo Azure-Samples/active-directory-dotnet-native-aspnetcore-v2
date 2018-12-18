75a78ec2fbb918e738b8484575140beec09ee634
George Krechar <dokrecar@microsoft.com>
2018-12-18 13:44:48 -0800

    Upgrade to netcoreapp v2.2.0

--- 1. Desktop app calls Web API/TodoListService/Startup.cs
+++ 1. Desktop app calls Web API/TodoListService/Startup.cs
@@ -61,7 +61,7 @@ namespace TodoListService
 
 
             });
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
