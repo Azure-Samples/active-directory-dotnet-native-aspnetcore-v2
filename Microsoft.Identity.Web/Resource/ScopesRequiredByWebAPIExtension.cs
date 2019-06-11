b8d3cbd63829bd87e4d71c40026d2780153573fc
Jean-Marc Prieur <jmprieur@microsoft.com>
2019-06-11 17:21:53 +0200

    Addressing the PR feedback

--- Microsoft.Identity.Web/Resource/ScopesRequiredByWebAPIExtension.cs
+++ Microsoft.Identity.Web/Resource/ScopesRequiredByWebAPIExtension.cs
@@ -13,12 +13,17 @@ namespace Microsoft.Identity.Web.Resource
     public static class ScopesRequiredByWebAPIExtension
     {
         /// <summary>
-        /// When applied to a Controller, verifies that the user authenticated in the Web API has any of the
-        /// accepted scopes
+        /// When applied to an <see cref="HttpContext"/>, verifies that the user authenticated in the Web API has any of the
+        /// accepted scopes. If the authentication user does not have any of these <paramref name="acceptedScopes"/>, the
+        /// method throws an HTTP Unauthorized with the message telling which scope are missing
         /// </summary>
         /// <param name="acceptedScopes">Scopes accepted by this API</param>
         public static void VerifyUserHasAnyAcceptedScope(this HttpContext context, IEnumerable<string> acceptedScopes)
         {
+            if (acceptedScopes == null)
+            {
+                throw new ArgumentNullException("acceptedScopes");
+            }
             Claim scopeClaim = context.User.FindFirst("http://schemas.microsoft.com/identity/claims/scope");
             if (scopeClaim == null || !scopeClaim.Value.Split(' ').Intersect(acceptedScopes).Any())
             {
@@ -30,12 +35,17 @@ namespace Microsoft.Identity.Web.Resource
 
         /// <summary>
         /// When applied to a controller, verifies that the user on behalf of which the Web API is called
-        /// has the required scope
+        /// has the required scope. If the authentication user does not have any of these <paramref name="requiredScope"/>, the
+        /// method throws an HTTP Unauthorized with the message telling which scope are missing
         /// </summary>
-        /// <param name="requireScope">Scope required to call the API</param>
-        public static void VerifyUserHasRequiredScope(this HttpContext context, string requireScope)
+        /// <param name="requiredScope">Scope required to call the API</param>
+        public static void VerifyUserHasRequiredScope(this HttpContext context, string requiredScope)
         {
-            context.VerifyUserHasAnyAcceptedScope(new[] { requireScope });
+            if (requiredScope == null)
+            {
+                throw new ArgumentNullException("requireScope");
+            }
+            context.VerifyUserHasAnyAcceptedScope(new[] { requiredScope });
         }
     }
 }
