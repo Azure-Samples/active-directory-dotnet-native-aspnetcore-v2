7a77e4a36b33597ecb1667656663067b0ee3f189
Jean-Marc Prieur <jmprieur@microsoft.com>
2019-09-11 09:31:16 -0700

    Fixing an XML comment

--- 2. Web API now calls Microsoft Graph/TodoListService/Controllers/TodoListController.cs
+++ 2. Web API now calls Microsoft Graph/TodoListService/Controllers/TodoListController.cs
@@ -36,7 +36,7 @@ namespace TodoListService.Controllers
         static readonly ConcurrentBag<TodoItem> TodoStore = new ConcurrentBag<TodoItem>();
 
         /// <summary>
-        /// /// The Web API will only accept tokens 1) for users, and
+        /// The Web API will only accept tokens 1) for users, and
         /// 2) having the access_as_user scope for this API
         /// </summary>
         static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
